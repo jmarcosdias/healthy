3f5cb68a6e7b3074d67c552de81931dded5ac484
jmarcosdias <jmsdias6@gmail.com>
2022-09-14 00:04:59 +0000

    Create the home app

---
+++ home/urls.py
@@ -0,0 +1,10 @@
+"""
+URLs configuration for home app
+"""
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='home'),
+]
---
+++ home/views.py
@@ -0,0 +1,12 @@
+"""
+Views for the home app
+"""
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def index(request):
+    """ A view to return the index page """
+
+    return render(request, 'home/index.html')
