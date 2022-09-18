51b809b210b609e6784e969601627af571e589cc
jmarcosdias <jmsdias6@gmail.com>
2022-09-18 16:41:54 +0000

    Fix an issue with the order total

--- checkout/apps.py
+++ checkout/apps.py
@@ -1,6 +1,14 @@
+"""
+Application configurations for the checkout app
+"""
+
+
 from django.apps import AppConfig
 
 
 class CheckoutConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'checkout'
+
+    def ready(self):
+        import checkout.signals
