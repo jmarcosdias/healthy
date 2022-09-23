23f44f1f85043549c8056fa147fd142f7548f401
jmarcosdias <jmsdias6@gmail.com>
2022-09-23 13:25:09 +0000

    Complete the newsletter unsubscription
    It is now possible for the user to unsubscribe from the newsletter.

--- newsletter/urls.py
+++ newsletter/urls.py
@@ -7,6 +7,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.subscribe_newsletter, name='subscribe_newsletter'),
-    # path('', views.unsubscribe_newsletter, name='unsubscribe_newsletter'),
+    path('subscribe/', views.subscribe, name='subscribe_newsletter'),
+    path('unsubscribe/', views.unsubscribe, name='unsubscribe_newsletter'),
+    path('unsubscribe/<email>', views.unsubscribe_email,
+         name='unsubscribe_newsletter_email'),
 ]
--- newsletter/views.py
+++ newsletter/views.py
@@ -2,13 +2,14 @@
 Views for the newsletter app
 """
 
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.http import Http404
 from django.contrib import messages
 from .models import NewsletterSubscription
 from .forms import NewsletterForm
 
 
-def subscribe_newsletter(request):
+def subscribe(request):
     """ A view to handle newsletter subscription """
     form = NewsletterForm()
 
@@ -19,6 +20,9 @@ def subscribe_newsletter(request):
             messages.success(request, 'Thank you for subscribing to \
                  our newsletter!')
             return redirect(reverse('home'))
+    
+    messages.error(request, 'Sorry, we were unable to fulfill your reqest. \
+        Please review your input!')
 
     context = {
         'form': form,
@@ -26,3 +30,34 @@ def subscribe_newsletter(request):
 
     return render(request, 'newsletter/subscribe_newsletter.html', context)
 
+
+def unsubscribe(request):
+    """ A view to handle newsletter unsubscription """
+    form = NewsletterForm()
+
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        return redirect(reverse('unsubscribe_newsletter_email', args=[email]))
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'newsletter/unsubscribe_newsletter.html', context)
+
+
+def unsubscribe_email(request, email):
+    """ A view to unsubscribe a specific email """
+    try:
+        newsletter_subscription = get_object_or_404(NewsletterSubscription,
+                                                    email=email)
+        newsletter_subscription.delete()
+        messages.success(request, f'We have unsubscribed {newsletter_subscription.email} \
+            from our newsletter!')
+        return redirect(reverse('home'))
+
+    except Http404:       
+        messages.error(request, f'Sorry, we are not finding this email. \
+            Are you sure you subscribed {email} before?')
+
+        return redirect(reverse('unsubscribe_newsletter'))
